50a9f9e42f5849e00858529532789f9251efa19d
Olaf Meyer <olaf.meyer@scotec-software.com>
2024-03-16 01:09:37 +0100

    Print running status of default process control to console.

--- Source/Controller/SignalF.Controller/Signals/ProcessControl/DefaultProcedure.cs
+++ Source/Controller/SignalF.Controller/Signals/ProcessControl/DefaultProcedure.cs
@@ -8,10 +8,25 @@ public class DefaultProcedure : IProcessControlProcedure
 {
     public async Task Main(IProcessControlAdapter processControlAdapter, CancellationToken cancellationToken)
     {
+        var count = 0;
+        Console.WriteLine();
+
         while (true)
         {
-            Console.WriteLine("Default precess is running");
-            await Task.Delay(1000, cancellationToken);
+            if (count++ % 10 == 0)
+            {
+                var currentLineCursor = Console.CursorTop;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLineCursor);
+                Console.Write("\rDefault process is running " );
+            }
+            else
+            {
+                Console.Write(".");
+            }
+            await Task.Delay(500, cancellationToken);
         }
+        // ReSharper disable once FunctionNeverReturns
     }
 }
