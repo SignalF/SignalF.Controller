876b2429bef3596558f9a8e657116f4bab97800f
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-03-18 12:05:22 +0100

    Show dynamic status text.

--- Source/Controller/SignalF.Controller/Signals/ProcessControl/DefaultProcedure.cs
+++ Source/Controller/SignalF.Controller/Signals/ProcessControl/DefaultProcedure.cs
@@ -8,10 +8,35 @@ public class DefaultProcedure : IProcessControlProcedure
 {
     public async Task Main(IProcessControlAdapter processControlAdapter, CancellationToken cancellationToken)
     {
+        Console.WriteLine();
+
+        int count = 0;
         while (true)
         {
-            Console.WriteLine("Default precess is running");
-            await Task.Delay(1000, cancellationToken);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                break;
+            }
+
+            if (count++ % 10 == 0)
+            {
+                ClearCurrentConsoleLine();
+                Console.Write("Default process is running ");
+            }
+            else
+            {
+                Console.Write(".");
+            }
+
+            await Task.Delay(500, cancellationToken);
         }
+    }
+
+    public static void ClearCurrentConsoleLine()
+    {
+        int currentLineCursor = Console.CursorTop;
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Write(new string(' ', Console.WindowWidth));
+        Console.SetCursorPosition(0, currentLineCursor);
     }
 }
