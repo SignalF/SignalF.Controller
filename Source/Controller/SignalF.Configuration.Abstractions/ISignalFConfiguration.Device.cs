abfb43f88da9a9d5177ec0412561b29f33a37887
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-08-01 17:09:35 +0200

    Fixed assembly reference. Use generics type of configuration builder is constraint.

--- Source/Controller/SignalF.Configuration.Abstractions/ISignalFConfiguration.Device.cs
+++ Source/Controller/SignalF.Configuration.Abstractions/ISignalFConfiguration.Device.cs
@@ -17,20 +17,20 @@ public partial interface ISignalFConfiguration
         where TType : class, IDevice;
 
     ISignalFConfiguration AddDeviceDefinition<TBuilder, TOptions>(Action<TBuilder> builder)
-        where TBuilder : IDeviceDefinitionBuilder
+        where TBuilder : IDeviceDefinitionBuilder<TBuilder, IDeviceDefinition, TOptions>
         where TOptions : SignalFConfigurationOptions;
 
     ISignalFConfiguration AddDeviceDefinition<TBuilder, TOptions, TType>(Action<TBuilder> builder)
-        where TBuilder : IDeviceDefinitionBuilder
+        where TBuilder : IDeviceDefinitionBuilder<TBuilder, IDeviceDefinition, TOptions>
         where TOptions : SignalFConfigurationOptions
         where TType : class, IDevice;
 
     ISignalFConfiguration AddDeviceTemplate<TBuilder, TOptions>(Action<TBuilder> builder)
-        where TBuilder : IDeviceTemplateBuilder
+        where TBuilder : IDeviceTemplateBuilder<TBuilder, IDeviceTemplate, TOptions>
         where TOptions : SignalFConfigurationOptions;
 
     ISignalFConfiguration AddDeviceTemplate<TBuilder, TOptions, TType>(Action<TBuilder> action)
-        where TBuilder : IDeviceTemplateBuilder
+        where TBuilder : IDeviceTemplateBuilder<TBuilder, IDeviceTemplate, TOptions>
         where TOptions : SignalFConfigurationOptions
         where TType : class, IDevice;
 }
