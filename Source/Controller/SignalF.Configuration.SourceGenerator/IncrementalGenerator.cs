ab1dcebbf18b3cabfd5e43bad76f34f1072019da
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-08-02 12:07:45 +0200

    Code cleanup.

--- Source/Controller/SignalF.Configuration.SourceGenerator/IncrementalGenerator.cs
+++ Source/Controller/SignalF.Configuration.SourceGenerator/IncrementalGenerator.cs
@@ -1,39 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
-using System.Text;
+﻿using System.Reflection;
 using Microsoft.CodeAnalysis;
 
-namespace SignalF.Configuration.SourceGenerator
+namespace SignalF.Configuration.SourceGenerator;
+
+public abstract class IncrementalGenerator : IIncrementalGenerator
 {
-    public abstract class IncrementalGenerator : IIncrementalGenerator
+    protected IncrementalGeneratorInitializationContext Context { get; private set; }
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        protected IncrementalGeneratorInitializationContext Context { get; private set; }
-
-        public void Initialize(IncrementalGeneratorInitializationContext context)
-        {
-            Context = context;
-            OnInitialize();
-        }
-
-        protected static string? LoadTemplate(string templateName)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourcePath = assembly
-                               .GetManifestResourceNames()
-                               .FirstOrDefault(name => name.Contains(templateName));
-
-            if (resourcePath == null)
-            {
-                return null;
-            }
-
-            using var stream = assembly.GetManifestResourceStream(resourcePath)!;
-            using var reader = new StreamReader(stream);
-            return reader.ReadToEnd();
-        }
-
-        protected abstract void OnInitialize();
+        Context = context;
+        OnInitialize();
     }
+
+    protected static string? LoadTemplate(string templateName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourcePath = assembly
+                           .GetManifestResourceNames()
+                           .FirstOrDefault(name => name.Contains(templateName));
+
+        if (resourcePath == null)
+        {
+            return null;
+        }
+
+        using var stream = assembly.GetManifestResourceStream(resourcePath)!;
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+
+    protected abstract void OnInitialize();
 }
