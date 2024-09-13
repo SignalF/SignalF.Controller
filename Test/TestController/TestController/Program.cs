c8fb19ab60a5cef5f0536ac75de0580a1d3ec56c
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-09-13 17:13:51 +0200

    Updated packages.

--- Test/TestController/TestController/Program.cs
+++ Test/TestController/TestController/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.Runtime.Versioning;
+﻿using System.IO.Packaging;
+using System.Runtime.Versioning;
+using Microsoft.Extensions.Hosting;
+using Scotec.XMLDatabase;
 using SignalF.Controller;
 using SignalF.Controller.Configuration;
+using SignalF.Datamodel.Configuration;
 using SignalF.Extensions.Configuration;
 using SignalF.Extensions.Controller;
 
@@ -40,6 +44,24 @@ public class Program
 
         var host = hostBuilder.Build();
 
+        Test(host.Services);
+
         await host.RunAsync();
+    }
+
+    private static void Test(IServiceProvider services)
+    {
+        //LoadSchema(new Uri("pack://application:,,,ConsoleApp1;component/Schemas/SignalF.Datamodel.Configuration.xsd"), schemas);
+        var document = services.GetService<IBusinessDocument>();
+        document.Root = "ControllerConfiguration";
+
+        //Test();
+
+        var scheme = PackUriHelper.UriSchemePack;
+        document.Schema = new Uri($"{scheme}://application:,,,SignalF.Datamodel;component/Schemas/SignalF.Datamodel.Configuration.xsd");
+        document.CreateDocument("");
+        var session = document.CreateSession(EBusinessSessionMode.Write);
+        var configuration = session.GetRoot<IControllerConfiguration>();
+
     }
 }
