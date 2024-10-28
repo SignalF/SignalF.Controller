46653621cbfa73fe199e7dc809b8bef6ba118a24
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-10-28 23:36:34 +0100

    Renamed position to Point.cs Added links. Added signal connection reference.

---
+++ Source/Datamodel/SignalF.Datamodel.Designer.Abstractions/IPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Scotec.XMLDatabase;
+
+
+namespace SignalF.Datamodel.Designer
+{
+	public partial interface IPoint : IBusinessObject, Scotec.XMLDatabase.IVisitable
+	{
+
+		#region Properties
+
+		System.Double X { get; set; }
+
+		System.Double Y { get; set; }
+
+		#endregion Properties
+
+
+		#region Methods
+
+		#endregion Methods
+
+	}
+
+	public interface IPointVisitor<T> : Scotec.XMLDatabase.IVisitor<T>
+	{
+		T Visit(IPoint visitable);
+	}
+}
+
---
+++ Source/Datamodel/SignalF.Datamodel.Designer.Abstractions/IPointList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+using Scotec.XMLDatabase;
+
+
+namespace SignalF.Datamodel.Designer
+{
+	public partial interface IPointList : IBusinessObjectList<SignalF.Datamodel.Designer.IPoint>
+	{
+
+	}
+}
+
